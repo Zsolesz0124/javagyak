c60a97f0b7543f66426569a185c13f31fd0b1ffe
zsxlesz <136483423+zsxlesz@users.noreply.github.com>
2023-11-22 13:00:15 +0100

    initial commit
    struktúra kialakítása, index és bejelentkezés elkezdése

---
+++ beégetett_felhasznalok.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (`id`, `email`, `name`, `password`) VALUES
+(1,'proba@proba','Proba User', '$2a$10$T5BNa4JdSFx0ZFUgYVRANuS6RvYWyrrYoQW.Q/t/jMnp5cv3BobBS'),
+(2,'admin@admin','Admin', '$2a$10$T5BNa4JdSFx0ZFUgYVRANuS6RvYWyrrYoQW.Q/t/jMnp5cv3BobBS');
