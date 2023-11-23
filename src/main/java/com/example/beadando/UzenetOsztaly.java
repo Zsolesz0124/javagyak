0528085d4736a124ae32fa30099ff3856b1639d5
email <szbfrnc7@gmail.com>
2023-11-23 10:44:10 +0100

    uzenetosztaly

---
+++ src/main/java/com/example/beadando/UzenetOsztaly.java
@@ -0,0 +1,35 @@
+package com.example.beadando;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "uzenet_osztaly")
+public class UzenetOsztaly {
+    private static int count = 0;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Size(min=2, max=500)
+    private String content;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+}
